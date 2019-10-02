442a11ead88bd270f7c1bd3be9ce56f81be4c2c0
knack210 <nickderosa@knights.ucf.edu>
2019-10-02 19:34:19 -0400

    Rough Movement

---
+++ SuperSprint/Assets/Script/CharacterController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public bool isRunning;
+    public float speed;
+    public GameObject goal;
+
+    private Vector2 goalLocation;
+
+    private void Start()
+    {
+        goalLocation = goal.transform.position;    
+    }
+
+    private void Update()
+    {
+        if (isRunning)
+        {
+            this.transform.position = Vector2.MoveTowards(transform.position, goalLocation, speed * Time.deltaTime);
+        }
+    }
+}
