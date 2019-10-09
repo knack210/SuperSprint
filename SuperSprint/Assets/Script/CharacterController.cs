76c5c88b1b3f0481067ae9a237700e679286ca2b
knack210 <nickderosa@knights.ucf.edu>
2019-10-08 22:41:23 -0400

    Rough sliding

--- SuperSprint/Assets/Script/CharacterController.cs
+++ SuperSprint/Assets/Script/CharacterController.cs
@@ -5,18 +5,23 @@ using UnityEngine;
 public class CharacterController : MonoBehaviour
 {
     private bool isRunning;
+    private bool isSliding;
     [SerializeField]
     private float speed;
     [SerializeField]
     private float jumpHeight;
     [SerializeField]
+    private float slideTime;
+    [SerializeField]
     private GameObject goal;
     [SerializeField]
     private LayerMask groundLayer;
+    
 
     private Rigidbody2D rb;
     private Vector2 goalLocation;
     private Transform GroundTouch;
+    private Animator anim;
     [SerializeField]
     private BoxCollider2D standCol;
     [SerializeField]
@@ -25,26 +30,35 @@ public class CharacterController : MonoBehaviour
     private void Start()
     {
         isRunning = true;
+        anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
         goalLocation = goal.transform.position;
         GroundTouch = this.transform.Find("GroundTouch");
         slideCol.enabled = false;
     }
 
-    private void Update()
+    private void LateUpdate()
     {
+        
+
         if (isRunning)
         {
-            this.transform.position = Vector2.MoveTowards(transform.position, goalLocation, speed * Time.deltaTime);
+            this.transform.position = Vector2.MoveTowards(transform.position, goalLocation, speed * Time.deltaTime);           
         }
 
-        if (Input.GetButtonDown("Jump") && IsOnGround())
+        if (IsOnGround())
+        {
+            anim.SetBool("isJump", false);
+        }
+        
+        if (Input.GetButtonDown("Jump") && IsOnGround() && !isSliding)
         {
             Debug.Log("Grounded");
+            anim.SetBool("isJump", true);
             rb.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
         }
 
-        if (Input.GetKeyDown(KeyCode.DownArrow) && IsOnGround())
+        if (Input.GetKeyDown(KeyCode.DownArrow) && IsOnGround() && !isSliding)
         {
             slideStart();
         }
@@ -59,9 +73,11 @@ public class CharacterController : MonoBehaviour
     private void slideStart()
     {
         Debug.Log("Sliding");
+        anim.SetBool("isSlide", true);
+        isSliding = true;
         standCol.enabled = false;
         slideCol.enabled = true;
-        Invoke("slideEnd", 2);
+        Invoke("slideEnd", slideTime);
     }
 
     private void slideEnd()
@@ -69,5 +85,7 @@ public class CharacterController : MonoBehaviour
         Debug.Log("Not sliding");
         standCol.enabled = true;
         slideCol.enabled = false;
+        isSliding = false;
+        anim.SetBool("isSlide", false);
     }
 }
