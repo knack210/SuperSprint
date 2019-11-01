1fe9bd77e3b96615de8a3cb28c1efe65d7013d74
JohnDBass <48814518+JohnDBass@users.noreply.github.com>
2019-11-01 01:12:25 -0400

    Started some swipe scripts

---
+++ SuperSprint/Assets/Script/SwipeLogger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipeLogger : MonoBehaviour
+{
+    private void Awake()
+    {
+        SwipeDetect.OnSwipe += SwipeDetect_OnSwipe;
+    }
+
+    private void SwipeDetect_OnSwipe(SwipeData data)
+    {
+        Debug.Log("Swipe in Direction: " + data.Direction);
+    }
+}
