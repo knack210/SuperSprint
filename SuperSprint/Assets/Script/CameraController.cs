af3e40bb1ef42a85cac8c2d8c070335c3947d20d
maxchiappa <maxchiappa95@gmail.com>
2019-11-03 00:40:45 -0400

    distancemeter,scaling,
    hi

--- SuperSprint/Assets/Script/CameraController.cs
+++ SuperSprint/Assets/Script/CameraController.cs
@@ -5,22 +5,32 @@ using UnityEngine.UI;
 
 public class CameraController : MonoBehaviour
 {
-	[SerializeField]
-	private GameObject follow;
+    [SerializeField]
+    private GameObject follow;
     [SerializeField]
     private GameObject end;
     [SerializeField]
     private Text distanceText;
+
+
+    //maxxxxxxxxxxxxxxxxxxxxxx
+    [SerializeField]
+    public Slider distanceSlider;
 
     private float offset;
     private float endPoint;
 
     // Sets object to follow and endpoint
     private void Start()
-    {
+    {//maxxxxxxxxxxxxxxxxxxxxxxxxxx
+        distanceSlider.value = 0;
+
         offset = transform.position.x - follow.transform.position.x;
         endPoint = end.transform.position.x;
         UpdateText();
+
+
+
     }
 
     // Follows assigned object until endpoint is reached
@@ -31,14 +41,18 @@ public class CameraController : MonoBehaviour
             transform.position = new Vector3(follow.transform.position.x + offset, transform.position.y, transform.position.z);
             UpdateText();
         }
+        distanceSlider.value = Mathf.RoundToInt(endPoint - follow.transform.position.x);
     }
-        
+
     private void UpdateText()
     {
         int distance = Mathf.RoundToInt(endPoint - follow.transform.position.x);
         distanceText.text = "Remaining distance: " + distance.ToString() + " >";
-    } 
-    
+
+        //maxxxxxxxxxxxxxxxxxx
+        // distanceSlider.value = offset;
+    }
+
     private void DisableText()
     {
         distanceText.enabled = false;
