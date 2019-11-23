54bf8d522e2fea9bb20f9b1609f9463692e14bc0
knack210 <nickderosa@knights.ucf.edu>
2019-11-23 18:18:01 -0500

    Better game feel + parallax

---
+++ SuperSprint/Assets/Script/ParallaxBackground.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxBackground : MonoBehaviour
+{
+	private float length, startPoint;
+	private Camera _camera;
+	[SerializeField] private float parallaxSpeed;
+   
+    private void Start()
+    {
+		_camera = Camera.main;
+		startPoint = transform.position.x;
+		length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+	private void Update()
+    {
+		float temp = (_camera.transform.position.x * (1 - parallaxSpeed));
+		float distance = (_camera.transform.position.x * parallaxSpeed);
+
+		transform.position = new Vector3(startPoint + distance, transform.position.y, transform.position.z);
+
+		if (temp > startPoint + length) startPoint += length;
+		else if (temp < startPoint - length) startPoint -= length;
+    }
+}
