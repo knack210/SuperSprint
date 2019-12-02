bd38262b6a3bce27a26f036562b41f892a5b2b94
maxchiappa <maxchiappa95@gmail.com>
2019-12-01 21:41:21 -0500

    areyousurepanel when deletingdata

---
+++ SuperSprint/Assets/Script/AreYouSureToggle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreYouSureToggle : MonoBehaviour
+{
+    public GameObject Panel;
+
+    public void OpenPanel()
+    {
+        if (Panel != null)
+        {
+            Panel.SetActive(true);
+        }
+    }
+
+    public void ClosePanel()
+
+    {
+        if (Panel !=null)
+        {
+            Panel.SetActive(false);
+        }
+    }
+}
